c0219ebb30f246c18140aa757ce4544dd940d5d0
Hana Um <hanau12@gmail.com>
2015-08-22 19:25:35 -0700

    can register users

--- client/factories/mainFactory.js
+++ client/factories/mainFactory.js
@@ -1,11 +1,12 @@
-ourApp.factory('ourApp', function ($http) {
+ourApp.factory('mainFactory', function ($http) {
   var users = [];
   var factory = {};
 
-  factory.logIn = function (username, callback) {
-    $http.post('/user/login', username).then( function (response) {
-      user = response.data;
-      callback();
+  factory.addUser = function (userdata, callback) {
+    console.log(userdata);
+    $http.post('/addUser', userdata).success(function(data) {
+      user = data;
+      callback(user);
     });
   }
   return factory;
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-mongoose.connect('mongodb://jkhh:jkhh123@ds035553.mongolab.com:35553/mocproject');
+mongoose.connect('mongodb://jkhh:jkhh123@ds035553.mongolab.com:35553/mocproject
+');
 var models_path = __dirname + '/../server/models';
-
+mongodb://jkhh:jkhh123@ds035553.mongolab.com:35553/mocproject
 fs.readdirSync(models_path).forEach( function (file) {
   if (file.indexOf('.js') > 0) {
     require(models_path + '/' + file);
